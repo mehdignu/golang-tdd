54114f9f3ed01edb6a8a9147b023b12e5d5de697
mehdi <mehdi.dridi@g-portal.com>
2024-08-14 15:21:29 +0200

    custom types

---
+++ methods/shapes_test.go
@@ -0,0 +1,20 @@
+package methods
+
+import "testing"
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+		}
+	}
+}
